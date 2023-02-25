0d1a3bedab4526dee25d3a3f730d80973597317f
tharaka <tharakahitibandara@gmail.com>
2023-02-25 14:21:08 +0530

    the main two components were setup

---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Main() {
+    return (
+        <div className="Main">
+            <h1>Fun Facts About React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has over 100k stats on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprises apps, including mobile apps</li>
+            </ul>
+        
+        </div>
+    );
+    }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+export default function Navbar() {
+
+    return (
+        <div className="Navbar">
+            <div className="Navbar-left">
+                
+                <h1>ReactFacts</h1>
+            </div>
+
+            <div className="Navbar-right">
+                <h2>React Course - Project 1</h2>
+
+            </div>
+
+        </div>
+    );
+}
